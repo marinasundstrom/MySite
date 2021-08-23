0212fc5816af43136408f615c58bbd20f7aacfb6
Robert Sundström <robert.sundstrom@outlook.com>
2021-08-23 20:26:04 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Statiq.App;
+using Statiq.Web;
+using Statiq.Common;
+
+DateTimeFormatInfo dtfi = new DateTimeFormatInfo();
+dtfi.ShortDatePattern = "yyyy/MM/dd";
+CultureInfo ci = new CultureInfo(CultureInfo.CurrentCulture.Name);
+ci.DateTimeFormat = dtfi;
+
+CultureInfo.CurrentCulture = ci;
+
+await Bootstrapper
+    .Factory
+    .CreateWeb(args)
+    .DeployToGitHubPages(
+        "robertsundstrom",
+        "robertsundstrom.github.io",
+        Config.FromSetting<string>("GITHUB_TOKEN")
+    )
+    .RunAsync();
---
+++ input/posts/hello-world.md
@@ -0,0 +1,23 @@
+---
+title: "Hello, World!"
+published: 2019-05-23
+author: Robert Sundström"
+tags: ["hello-world"]
+---
+
+This is my first post!
+
+```csharp
+using System;
+
+namespace HelloWorld
+{
+    class Program 
+    {
+        static void Main() 
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+```
